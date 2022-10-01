fb1a98c099709a3e8b3ada1b4174676221647bf0
Gaoxin_Huang <gx173139880@qq.com>
2022-10-01 23:45:26 +1300

    part 4 Complete masstransit

---
+++ Youtube/InventoryService/InventoryService/OrderConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Model;
+
+internal class OrderConsumer : IConsumer<Order>
+{
+    private readonly ILogger<OrderConsumer> _logger;
+
+    public OrderConsumer(ILogger<OrderConsumer> logger) // 查看 OrderConsumer 的 DI 
+    {
+        this._logger = logger;
+    }
+    public async Task Consume(ConsumeContext<Order> context)
+    {
+        await Console.Out.WriteLineAsync(context.Message.Name);
+        _logger.LogInformation($"Test Log");
+    }
+}
--- Youtube/OrderService/OrderService/Program.cs
+++ Youtube/OrderService/OrderService/Program.cs
@@ -10,6 +10,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region 用 MassTransit DI
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host("amqp://guest:guest@localhost:5672");
+    });
+});
+//builder.Services.AddMassTransitHostedService(); // 貌似已经被 Obsolete 或 Deprecated, 现在 hosted service 会自动加到 container 里
+#endregion
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,21 +34,24 @@ if (app.Environment.IsDevelopment())
 #region Basic code
 //var bus = Bus.Factory.CreateUsingRabbitMq(config =>
 //{
-//config.Host("amqp://guest:guest@localhost:5672");
-//config.ReceiveEndpoint("temp-queue", c =>
-//{
-//c.Handler<Order>(ctx =>
-//{
-//    return Console.Out.WriteLineAsync(ctx.Message.Name);
-//});
-//});
+//    config.Host("amqp://guest:guest@localhost:5672");
+//    config.ReceiveEndpoint("temp-queue", c =>
+//    {
+//        c.Handler<Order>(ctx =>
+//        // 注: 这里会自动生成一个 top level 的 exchange , 以这个 Order 来命名(这里就是 Model.Order)。所以推荐这个 model 最好变成共享lib
+//        // 然后这个 exchange 会绑定一个 "temp-queue" 这个 exchange (同样是自动生成的, 根据 queue的名字),  而且消息传给 temp-queue 这个 exchange
+//        // 然后这个 "temp-queue" exchang 会再去binding  temp-queue,  把这个消息传给 这个queue
+//        {
+//            return Console.Out.WriteLineAsync(ctx.Message.Name);
+//        });
+//    });
 //});
 
 //bus.Start();
 //bus.Publish(new Order
 //{
-//    Name = "Test"
-//}); 
+//    Name = "Test name"
+//});
 #endregion
 
 app.UseHttpsRedirection();
